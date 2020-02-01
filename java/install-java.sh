a8db8a5330ea43d079f14d84d164eff614abaa9c
dtopuzov <dtopuzov@gmail.com>
2020-02-01 20:01:20 +0200

    fix: install java

--- java/install-java.sh
+++ java/install-java.sh
@@ -12,15 +12,11 @@
 
 source "$HOME"/.bash_profile
 
-# Reset variables
-sed -i '' '/JAVA_HOME/d' "$HOME"/.bash_profile
-echo "export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)" >>"$HOME"/.bash_profile
-
 # Install JDKs
 declare -a arr=("1.8" "11" "13")
 for i in "${arr[@]}"; do
   set +e
-  /usr/libexec/java_home -v "$i" > /dev/null 2>&1
+  /usr/libexec/java_home -v "$i" >/dev/null 2>&1
   EXIT_CODE=$?
   set -e
   if [ $EXIT_CODE == 0 ]; then
@@ -30,7 +26,7 @@ for i in "${arr[@]}"; do
     {
       # Handle Java 1.8 is acutally installed with adoptopenjdk8
       if [[ "$i" == "1.8" ]]; then
-        "$i"="8"
+        i="8"
       fi
 
       brew tap adoptopenjdk/openjdk
@@ -40,4 +36,10 @@ for i in "${arr[@]}"; do
   fi
 done
 
-source "$HOME"/.bash_profile
+# Reset variables
+echo "Set JAVA_HOME in ~/.bash_profile (point to JDK 8)."
+{
+  sed -i '' '/JAVA_HOME/d' "$HOME"/.bash_profile
+  echo "export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)" >>"$HOME"/.bash_profile
+  source "$HOME"/.bash_profile
+} &>"$HOME"/logs/install-java"$i".logs
