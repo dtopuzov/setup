afc3f73c69260407d3efca215372645a1ac33a2b
Dimitar Topuzov <dtopuzov@gmail.com>
2020-06-29 19:12:35 +0300

    feat: install browsers scripts

---
+++ browsers/install-chrome.sh
@@ -0,0 +1,11 @@
+echo "Install Google Chrome" {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Install Chrome not implemented on macOS"
+    else
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo dpkg -i google-chrome*.deb
+        sudo apt-get install -f
+        sudo apt autoremove
+	rm -rf *.deb
+    fi
+} &>"$HOME"/logs/install-chrome.logs
