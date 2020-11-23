448ec1e3f426368afb6a5ddf962084decfcdca4c
Dimitar Topuzov <dtopuzov@gmail.com>
2020-11-23 10:36:12 +0200

    chore: add android@11
    - Update Android SDK
    - Create Android 11 emulator

--- android/update-android-emulators.sh
+++ android/update-android-emulators.sh
@@ -24,6 +24,7 @@ update_images() {
   echo "Update Android emulator images."
   {
     download "--licenses"
+    download "system-images;android-30;google_apis_playstore;x86"
     download "system-images;android-29;google_apis_playstore;x86"
     download "system-images;android-28;google_apis_playstore;x86"
     download "system-images;android-27;google_apis_playstore;x86"
@@ -50,6 +51,7 @@ create() {
     echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n "PixelApi27" -k "system-images;android-27;google_apis_playstore;x86" -b "google_apis_playstore/x86" -c "256M" -f
     echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n "Pixel2Api28" -k "system-images;android-28;google_apis_playstore;x86" -b "google_apis_playstore/x86" -c "256M" -f
     echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n "Pixel3Api29" -k "system-images;android-29;google_apis_playstore;x86" -b "google_apis_playstore/x86" -c "256M" -f
+    echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n "Pixel4Api30" -k "system-images;android-30;google_apis_playstore;x86" -b "google_apis_playstore/x86" -c "256M" -f
   } &>$HOME/logs/create-android-emulators.logs
 }
 
@@ -66,6 +68,7 @@ configure() {
     configure_avd "PixelApi27" "Pixel"
     configure_avd "Pixel2Api28" "Pixel2"
     configure_avd "Pixel3Api29" "Pixel3"
+    configure_avd "Pixel4Api30" "Pixel4"
   } &>$HOME/logs/configure-android-emulators.logs
 }
 
