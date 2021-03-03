4261bd498fecdf921f96c0a2503694837c0aa5a5
Dimitar Topuzov <dtopuzov@gmail.com>
2021-03-03 14:30:27 +0200

    fix: java installation

--- java/install-java.sh
+++ java/install-java.sh
@@ -2,8 +2,8 @@
 
 ########################################################
 #
-# This script will install Open JDK 1.8,11 and 13.
-# Default in .bash_profile will be `Open JDK 11`.
+# This script will install Open JDK 1.8, 11 and 13.
+# Default in .bash_profile will be `Open JDK 1.8`.
 #
 ########################################################
 # shellcheck disable=SC1090
@@ -16,7 +16,8 @@ source "$HOME"/.bash_profile
 declare -a arr=("1.8" "11" "13")
 for i in "${arr[@]}"; do
   set +e
-  /usr/libexec/java_home -v "$i" >/dev/null 2>&1
+  VERSION="$i" | cut -d "." -f2
+  /usr/libexec/java_home -v "$i" | grep "$VERSION" >/dev/null 2>&1
   EXIT_CODE=$?
   set -e
   if [ $EXIT_CODE == 0 ]; then
@@ -24,15 +25,9 @@ for i in "${arr[@]}"; do
   else
     echo "Install Open JDK $i."
     {
-      # Handle Java 1.8 is actually installed with adoptopenjdk8
-      if [[ "$i" == "1.8" ]]; then
-        i="8"
-      fi
-
       brew tap adoptopenjdk/openjdk
-      brew install --cask adoptopenjdk"$i"
-      brew untap adoptopenjdk/openjdk
-    } &>"$HOME"/logs/install-java"$i".logs
+      brew install --cask adoptopenjdk"$VERSION"
+    } &>"$HOME"/logs/install-java.logs
   fi
 done
 
@@ -42,4 +37,4 @@ echo "Set JAVA_HOME in ~/.bash_profile (use JDK 1.8)."
   sed -i '' '/JAVA_HOME/d' "$HOME"/.bash_profile
   echo "export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)" >> "$HOME"/.bash_profile
   source "$HOME"/.bash_profile
-} &>"$HOME"/logs/install-java"$i".logs
+} &>"$HOME"/logs/install-java.logs
